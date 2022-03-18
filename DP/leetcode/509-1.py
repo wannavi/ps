58f07485044af64117cced2e0b7ecf24b0d77e03
Reeul <chotnt741@gmail.com>
2022-03-19 02:03:31 +0900

    :sparkles: 509 - Fibonacci Number
    O

---
+++ DP/leetcode/509-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def __init__(self):
+        self.dp = collections.defaultdict(int)
+
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+
+        if self.dp[n]:
+            return self.dp[n]
+
+        self.dp[n] = self.fib(n-1) + self.fib(n-2)
+        return self.dp[n]
